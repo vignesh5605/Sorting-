816d4c33801d7cac5ab532d18ea9bd49c2eb8a94
Vignesh5605 <vigneshramalingam305@gmail.com>
2025-12-19 17:09:54 +0530

    committed

---
+++ Sort.py
@@ -0,0 +1,25 @@
+n = int(input())
+arr = []
+
+for i in range(n):
+    arr.append(int(input()))
+
+low = 0
+mid = 0
+high = n - 1
+
+while mid <= high:
+    if arr[mid] == 0:
+        arr[low], arr[mid] = arr[mid], arr[low]
+        low += 1
+        mid += 1
+
+    elif arr[mid] == 1:
+        mid += 1
+
+    else:  # arr[mid] == 2
+        arr[mid], arr[high] = arr[high], arr[mid]
+        high -= 1
+
+print("Sorted array:")
+print(arr)
